d957ccfea3f861e91f007c4d1fd57051c948c4df
obellish <obellish@mpilabels.com>
2024-09-11 07:41:38 -0400

    remove sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { visualizer } from 'rollup-plugin-visualizer';
-import sassDts from 'vite-plugin-sass-dts';
 import vike from 'vike/plugin';
-import { resolve } from 'node:path';
 
 const debug = true;
 
@@ -13,20 +11,6 @@ export default defineConfig({
 		modules: {
 			generateScopedName: debug ? '[name]_[local]_[hash:base64:5]' : '[hash:base64:5]'
 		},
-		preprocessorOptions: {
-			scss: {
-				importer(...args: string[]) {
-					console.log(args);
-					if (args[0] !== '@/styles') {
-						return;
-					}
-
-					return {
-						file: resolve(__dirname, 'src', 'assets', 'styles')
-					}
-				}
-			}
-		}
 	},
 	clearScreen: false,
 	build: {
@@ -36,15 +20,6 @@ export default defineConfig({
 	},
 	plugins: [
 		react(),
-		sassDts({
-			enabledMode: ['development', 'production'],
-			global: {
-				generate: true,
-				outputFilePath: resolve(__dirname, 'src', 'style.d.ts')
-			},
-			sourceDir: resolve(__dirname, 'src'),
-			outputDir: resolve(__dirname, 'dist'),
-		}),
 		visualizer({
 			sourcemap: debug,
 		}),
